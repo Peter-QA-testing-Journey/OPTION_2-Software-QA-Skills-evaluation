cea83a3806553f613b81cc39221873b008d56320
Logo38 <peterbursa1@gmail.com>
2024-07-14 19:10:59 +0200

    change config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://docs.cypress.io',
+    screenshotOnRunFailure: false,
+    video: false,
+    supportFile: 'cypress/support/e2e.js',
   },
 });
